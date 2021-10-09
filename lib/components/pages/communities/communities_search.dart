e6d7d12fcd7cd69a61706e137a0a06b59e2555cf
diego04-rc <diego04-rc@users.noreply.github.com>
2021-10-09 15:53:24 +0200

    commit

---
+++ lib/components/pages/communities/communities_search.dart
@@ -0,0 +1,17 @@
+import 'package:eventy_front/objects/community.dart';
+import 'package:flutter/material.dart';
+
+class CommunitiesSearch extends StatefulWidget {
+  final List<Community> communitiesList;
+  const CommunitiesSearch(this.communitiesList) : super();
+
+  @override
+  _MyCommunitiesState createState() => _MyCommunitiesState();
+}
+
+class _MyCommunitiesState extends State<CommunitiesSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
