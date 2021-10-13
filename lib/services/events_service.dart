60d9c224128a59a84bbc8ee3f708cc60229d9ade
Airam Hernández Rocha <airamhr7@gmail.com>
2021-10-13 09:34:36 +0200

    Llamada a servidor

--- lib/services/events_service.dart
+++ lib/services/events_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:eventy_front/services/service.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
@@ -9,11 +11,17 @@ class EventService {
   String url = "";
 
   Future<List<Event>> get() async {
-    //sustituir por llamada a server
-    print("Here service");
-    final String response =
-        await rootBundle.loadString('mock_data/events.json');
-    final data = await json.decode(response) as List;
+    //sustituir por obtener localizacion
+    final query = {
+      'userId': '0',
+      'latitude': '39.357081',
+      'longitude': '-0.324842'
+    };
+    Uri url = Uri.http("localhost:8000", '/recomend', query);
+    final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
+    final localhostResponse = await http.get(url, headers: headers);
+    print("RESPONSE " + localhostResponse.body.toString());
+    final data = await json.decode(localhostResponse.body) as List;
     List<Event> events = data.map((event) => Event.fromJson(event)).toList();
     return events;
   }
