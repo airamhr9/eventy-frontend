9ddae2cc3d556343ca57e17e4a533dab414538b9
diego04-rc <diego.2000.c.r@gmail.com>
2021-10-14 12:14:47 +0200

    casi acabados los tags

--- lib/components/tags/tags_list.dart
+++ lib/components/tags/tags_list.dart
@@ -8,9 +8,27 @@ class TagsList extends StatefulWidget {
 }
 
 class _TagsListState extends State<TagsList> {
+  bool value = false;
+  List<Chip> tagList = [];
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Etiquetas"),
+        automaticallyImplyLeading: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView(
+              children: [
+                //...tagList.map(buildSingleTagCheckbox).toList()
+              ],
+            ),
+            TextButton(onPressed: () {}, child: Text("Aceptar")),
+          ],
+        ),
+      ),
+    );
   }
 }
