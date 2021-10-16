b99ed1357e2854649e1a6b852a39ffc78245f0bb
Jaime <jnegrillopastor@hotmail.com>
2021-10-16 17:39:01 +0200

    Merge ramas

--- lib/navigation/navigation.dart
+++ lib/navigation/navigation.dart
@@ -4,8 +4,7 @@ import 'package:eventy_front/components/pages/communities/community_view.dart';
 import 'package:eventy_front/components/pages/home/home.dart';
 import 'package:eventy_front/components/pages/my_events/my_events.dart';
 import 'package:eventy_front/components/pages/profile/profile.dart';
-import 'package:eventy_front/components/pages/search.dart';
-import 'package:eventy_front/components/pages/profile.dart';
+
 import 'package:eventy_front/components/search/search.dart';
 import 'package:flutter/material.dart';
 
