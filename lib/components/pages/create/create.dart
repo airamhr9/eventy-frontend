c5b4f50577b25881db63430af2d93c50228172db
airam <airamhr7@gmail.com>
2021-12-01 19:46:45 +0100

    pantalla crear

--- lib/components/pages/create/create.dart
+++ lib/components/pages/create/create.dart
@@ -13,19 +13,36 @@ class Create extends StatefulWidget {
 class _CreateState extends State<Create> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(children: [
-        FilledButton(
-          text: "Crear evento",
-          onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddEvent())),
-        ),
-        FilledButton(
-          text: "Crear comunidad",
-          onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddCommunity())),
-        )
-      ]),
-    );
+    return Column(children: [
+      SizedBox(
+        height: 130,
+      ),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: Image.asset('assets/images/create.png')),
+      SizedBox(
+        height: 50,
+      ),
+      Text(
+        "Crea momentos y conexiones",
+        style: TextStyle(fontSize: 16),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      FilledButton(
+        text: "Crear evento",
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AddEvent())),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      FilledButton(
+        text: "Crear comunidad",
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AddCommunity())),
+      )
+    ]);
   }
 }
